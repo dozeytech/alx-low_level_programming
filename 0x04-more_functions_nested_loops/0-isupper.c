5754baf6d8e173572d8959e53b5857568a310e3e
dozeytech <juchiprof40@gmail.com>
2023-02-23 03:18:02 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,9 +4,9 @@
  * @c: Variable text
  * Return: 1 if c is uppercase, 0 otherwise.
  */
-int_isupper(int c)
+int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
