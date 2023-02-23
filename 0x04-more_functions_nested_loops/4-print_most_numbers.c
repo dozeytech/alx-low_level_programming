560704bc076bdece0c665ba355538ac18a0f5ec1
dozeytech <juchiprof40@gmail.com>
2023-02-23 12:33:05 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,14 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int c;
 
-	for (i = 0; i <= 9; i++)
+	for (c = 0; c <= 9; c++)
 	{
-		if ((i == 2) || (i == 4))
+		if ((c == 2) || (c == 4))
 			continue;
 		else
-			_putchar(i + '0');
+			_putchar(c + '0');
 	}
 	_putchar('\n');
 }
