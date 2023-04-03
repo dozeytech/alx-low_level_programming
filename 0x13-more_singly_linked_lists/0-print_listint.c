4beb595c87c77298a736fbe2055077fa5d21fe95
dozeytech <juchiprof40@gmail.com>
2023-04-03 14:08:57 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,20 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t number = 0;
+	size_t j = 0;
 
-	while (h)
+	if (h != NULL)
 	{
+		while (h)
+		{
 
-	printf("%d\n", h->n);
-		number++;
-	h = h->next;
+		printf("%d\n", h->n);
+		j++;
+		h = h->next;
+		}
 
 	}
-	return (number);
+
+	return (j);
 }
 
