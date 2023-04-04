4c10ff37ead04b791ddf5d2a7fe48bea34e34f80
dozeytech <juchiprof40@gmail.com>
2023-04-04 12:25:43 -0700

    Updated

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that free a list
+ * @head: head of the list
+ * Return: nothing
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head)
+	{
+		while (*head)
+		{
+			temp = (*head);
+			*head = (*head)->next;
+			free(temp);
+		}
+
+	}
+	else
+	{
+		return;
+	}
+
+	free (*head);
+
+	head = 0;
+}
+
+
